a14a0ea19138f8a4ee26fe91806c30216346d461
BilalKarakan <bilalkaraka36@gmail.com>
2025-12-03 23:31:34 +0300

    The name was shown when the user logged in.

---
+++ Presentation/IdentityManagementSystem.Web/Views/Shared/_ShowUserName.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityManagementSystem.Web.Views.Shared
+{
+    public class _ShowUserNameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
